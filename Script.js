b95dcf9bb2d65c2868ebc7b6682699cb0c9420bf
Ayushtiwari2002 <98650309+Ayushtiwari2002@users.noreply.github.com>
2023-07-26 02:04:44 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+<><script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script><script>
+    var typed = new Typed('#element', {strings}: ['DIT University','Web Developer','Problem Solver'],
+    typeSpeed: 80,
+    loop: true,
+    {"}"});
+
+</script></>
